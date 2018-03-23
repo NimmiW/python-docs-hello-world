9d2ebf74c4302f547cf2172f7c5358b1c164dd41
NimmiW <nimmirashinika@gmail.com>
2018-03-23 15:13:09 +0530

    - add a plot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +11,58 @@ def hello_world():
 def html():
     return render_template('hello.html')
 
+@app.route('/chart/<pair>')
+def chart(pair):
+    return render_template('chart.html', pair = pair)
+
+@app.route('/plt')
+def plt():
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+    plt.plot(t, s)
+
+    plt.xlabel('time (s)')
+    plt.ylabel('voltage (mV)')
+    plt.title('About as simple as it gets, folks')
+    plt.grid(True)
+    plt.savefig("test.png")
+    return "done"
+
+@app.route("/simple.png")
+def simple():
+    import datetime
+    from io import BytesIO
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
+
+
+
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
