255bced04b4ff7a28e524511302e2bd0bff100e0
NimmiW <nimmirashinika@gmail.com>
2018-03-24 14:49:57 +0530

    - black regions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, make_response
 from brd import black_region_detection
+from anomalies import feature_selection, local_outlier_factor, local_outlier_factor_reducer, anomaly_identification
+
 
 app = Flask(__name__)
 
@@ -23,6 +25,23 @@ def brd(num):
 
 #--------------------------------------------------------anomalies Routes----------------------------------------------------#
 
+@app.route("/anomalies/selectfeatures")
+def feature_selecion():
+    return render_template('anomalies/feature_selection.html', status = feature_selection.feature_selecion())
+
+@app.route("/anomalies/detectlofmapper")
+def detect_lof_mapper():
+    return render_template('anomalies/local_outlier_factor_mapper.html', status = local_outlier_factor.detect_lof_mapper())
+
+@app.route("/anomalies/detectlofreducer")
+def detect_lof_reducer():
+    features = local_outlier_factor_reducer.detect_lof_reducer()
+    return render_template('anomalies/local_outlier_factor_reducer.html', features = features.to_html())
+
+@app.route("/anomalies/detectanomalies")
+def detect_anomalies():
+    anomalies = anomaly_identification.detect_anomalies()
+    return render_template('anomalies/anomalies.html', anomalies = anomalies.to_html())
 
 
 #--------------------------------------------------------bactesting Routes----------------------------------------------------#
